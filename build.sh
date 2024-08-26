b1741bddb10152db4f57af01fdc754b8688ebe29
Van Phan Mac Air <pvphat25@gmail.com>
2024-08-26 14:27:55 -0400

    dev draf build.sh

--- build.sh
+++ build.sh
@@ -29,6 +29,116 @@ docker rmi -f piggyfe
 docker rmi -f piggybe
 docker rmi -f piggydb
 
-echo "Success!\n\n"
+echo "Docker remove: Success!"
+echo "_______________________\n\n"
+
+# Database
+
+echo "BUILDING and RUNNING MYSQL DATBASE"
+
+cd database
+
+echo "Building database image"
+
+docker build -t piggydb .
+
+echo "Running the MySQL container"
+
+docker run -it -d -p 3306:3306 -v /piggydata/mysql/data:/var/lib/mysql --name piggydbctn piggydb
+
+docker ps
+
+echo "Database build and run SUCCESS! Live at port 3306"
+
+cd ..
+
+# Front end
+
+cd frontend
+
+echo "Piggy - FRONT END"
+
+echo "Building image"
+
+docker build -t piggyfe .
+
+echo "Running container"
+
+docker build -it -d --name piggyfectn piggyfe
+
+echo "DONE!"
+
+echo "Copy dist: Docker cp - COPYING dist to /var/html"
+
+docker cp piggyfectn:/app/dist /var/www/html/
+
+echo "DONE!"
+
+echo "STOPING the container front end"
+
+docker stop piggyfectn
+
+echo "REMOVE the container front end"
+
+docker rm -f piggyfectn
+
+echo "REMOVE the container image"
+
+docker rmi -f piggyfe
+
+echo "FRONT END build successfully\n\n"
+
+cd ..
+
+# Back end
+
+cd backend
+
+echo "Piggy - BACK END"
+
+echo "Building images"
+
+docker build -t piggybe .
+
+echo "DONE!"
+
+echo "Running container"
+
+docker run -it -d --name piggybectn piggybe
+
+echo "DONE!"
+
+echo "Copying the binary to the host"
+
+docker cp piggybectn:/app/piggybe-bin .
+
+echo "DONE!"
+
+echo "Stoping container"
+
+docker stop piggybectn
+
+echo "Removing container"
+
+docker rm -f piggybectn
+
+echo "Removing image"
+docker rmi piggybe
+
+echo "Restart the systemctl service"
+
+sudo systemctl restart piggybe-service.service
+
+echo "Backend Built and Run SUCCESSFULLY\n\n"
+
+cd .. 
+
+echo "FINISHED!"
+
+
+
+
+
+
 
 
