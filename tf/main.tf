d91aab653153215820cb3bf02fe6c5579b615341
van-xps-ubuntu <pvphat25@gmail.com>
2024-09-07 21:39:30 -0400

    edit tf

--- tf/main.tf
+++ tf/main.tf
@@ -11,6 +11,12 @@ provider "aws" {
   region = "us-east-2"
 }
 
+# get default VPC
+data "aws_vpc" "default" {
+	default = true;
+}
+
+
 resource "aws_instance" "piggy-ec2" {
   ami           = "ami-085f9c64a9b75eed5"
   instance_type = "t2.micro"
@@ -20,5 +26,5 @@ resource "aws_instance" "piggy-ec2" {
   }
 }
 
-
+# TO-DO: ADD RDS
 
